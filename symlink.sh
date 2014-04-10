3312914fdb49374e5b3ff6dce382d300cfe7394d
Tom Brow <tom@tombrow.com>
2014-04-10 11:22:26 -0700

    script to link files

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for f in `ls -1 -A | grep "^\." | grep -v "^\.git\(modules\)\?$"`
+do
+    echo "Linking $f"
+    ln -s "$PWD/$f" "$HOME/$f"
+done
+
+for f in Preferences/*
+do
+    echo "Linking $f"
+    ln -s "$PWD/$f" "$HOME/Library/$f"
+done
