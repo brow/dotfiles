84946a19e01d905e22eb3bb38cd1e9415715ecee
Tom Brow <tom@tombrow.com>
2020-02-12 14:46:25 -0600

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,19 @@
+
 function fish_prompt
-    echo "\$ "
+    echo "> "
 end
+
+# vi mode
+fish_vi_key_bindings
+
+function fish_mode_prompt
+end
+
+set fish_cursor_insert line
+
+# Disable greeting
+set fish_greeting
+
+# Suggested by homebrew
+set PATH /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/bin $PATH
+
