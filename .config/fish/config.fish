b12ca9b4b2f8d9c0bb65021dc4f9d0b1ab5f22a1
Tom Brow <tom@tombrow.com>
2020-06-12 07:29:07 -0500

    fish config tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,17 +3,18 @@ function fish_prompt
     echo "> "
 end
 
-# vi mode
-fish_vi_key_bindings
+# no vi mode
+fish_default_key_bindings
 
 function fish_mode_prompt
 end
 
 set fish_cursor_insert line
 
+set EDITOR vim
+
 # Disable greeting
 set fish_greeting
 
 # Suggested by homebrew
 set PATH /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/bin $PATH
-
