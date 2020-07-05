27d83a7da999e23d7f3bccdc8e81fdc08ba4d3f8
Tom Brow <tom@tombrow.com>
2020-07-04 21:41:38 -0500

    alias for terminal emacs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,3 +18,9 @@ set fish_greeting
 
 # Suggested by homebrew
 set PATH /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/bin $PATH
+
+# `emacs -nw` opens emacs in terminal rather than GUI
+# TERM varible makes themes work there:
+# https://github.com/syl20bnr/spacemacs/wiki/Terminal
+# https://github.com/syl20bnr/spacemacs/issues/1269
+alias enw="env TERM=xterm-24bit emacs -nw"
