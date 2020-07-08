b59292412dd7764e8ac72541e7454ea28b3daa44
Tom Brow <tom@tombrow.com>
2020-07-08 17:59:49 -0500

    fix symlink script

--- symlink.sh
+++ symlink.sh
@@ -2,6 +2,12 @@
 
 for f in `ls -1 -A | grep "^\." | grep -v "^\.git\(modules\)\?$"`
 do
-    echo "Linking $f"
-    ln -s "$PWD/$f" "$HOME/$f"
+    src="$PWD/$f"
+    dst="$HOME/$f"
+    if [ -f "$dst" ] || [ -d "$dst" ]; then
+        echo "Skipping $dst"
+    else
+        echo "ln -s $src $dst"
+        ln -s "$src" "$dst"
+    fi
 done
