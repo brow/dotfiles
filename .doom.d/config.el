9e4626519e805ebb4be58b856fb94fc89f087115
Tom Brow <tom@tombrow.com>
2020-09-20 12:20:16 -0500

    remove brow-org-copy-as-rich-text
    It was duplicative of org/export-to-clipboard-as-rich-text

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -65,21 +65,6 @@
       :map org-mode-map
       "l f" 'org-mac-safari-insert-frontmost-url)
 
-;; Adapted from John Kitchin's `formatted-copy`
-;; http://kitchingroup.cheme.cmu.edu/blog/2016/06/16/Copy-formatted-org-mode-text-from-Emacs-to-other-applications/
-(defun brow-org-copy-as-rich-text ()
-  "Copy org content as to clipboard as RTF."
-  (interactive)
-  (save-window-excursion
-    (let* ((buf (org-export-to-buffer 'html "*Temp buffer for brow-org-copy-as-rich-text*" nil nil t t))
-           (html (with-current-buffer buf (buffer-string))))
-      (with-current-buffer buf
-        (shell-command-on-region
-         (point-min)
-         (point-max)
-         "textutil -stdin -format html -convert rtf -stdout | pbcopy"))
-      (kill-buffer buf))))
-
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
