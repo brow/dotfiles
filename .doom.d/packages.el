05ff9cbc6ca931ab7c1bdecd03c60617109e24ae
Tom Brow <tom@tombrow.com>
2021-06-23 21:08:33 -0500

    install scad-mode

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -5,9 +5,8 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
-
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+(package! scad-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
