aefb67032c21be73cd985784cac78047defa38cf
Tom Brow <tom@tombrow.com>
2021-07-23 09:44:12 -0500

    add git-link package and some comments

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -5,8 +5,14 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;; Instead of using OpenSCAD's built-in editor:
 (package! scad-mode)
+
+;; Similar to `forge-browse-*' commands, but Forge doesn't have an equivalent of
+;; the eponymous `git-link' command.
+;; https://github.com/magit/forge/issues/91
+;; https://github.com/magit/forge/issues/91
+(package! git-link)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
