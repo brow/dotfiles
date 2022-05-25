a149e9cc2932e027ce3c5ea8c506cd394ae8d602
Tom Brow <tomb@squareup.com>
2022-05-25 13:37:01 -0500

    workaround for `doom install` fail

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -14,6 +14,15 @@
 ;; https://github.com/magit/forge/issues/91
 (package! git-link)
 
+;; Workaround for `doom install` failure
+;; https://github.com/doomemacs/doomemacs/issues/5667#issuecomment-948232137
+(package! gitconfig-mode
+	  :recipe (:host github :repo "magit/git-modes"
+			 :files ("gitconfig-mode.el")))
+(package! gitignore-mode
+	  :recipe (:host github :repo "magit/git-modes"
+			 :files ("gitignore-mode.el")))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
