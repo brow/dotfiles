2f547ff8b2a8f5c6e5cbdb5908f8eab5f8c7a12e
Tom Brow <tomb@squareup.com>
2022-05-25 12:52:46 -0500

    remove Nix stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,11 +18,3 @@ set fish_greeting
 
 # Suggested by homebrew
 set PATH /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/bin $PATH
-
-# "In the final steps of its installation, Nix sets up a few environment
-# variables in a bash script. I've adapted these specifically for fish on
-# macOS." -- https://duan.ca/2020/12/13/nix-on-macos-11-big-sur/
-set -x NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
-set -x NIX_SSL_CERT_FILE "$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt"
-set -x NIX_PATH /nix $HOME/.nix-defexpr/channels
-set -x PATH $HOME/.nix-profile/bin $PATH
