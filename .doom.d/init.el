2ad43a480731b04903c53d31de6c0a2290ccc8e0
Tom Brow <tomb@squareup.com>
2024-04-06 17:01:36 -0500

    add Emacs Everywhere

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -169,6 +169,7 @@
        ;;(wanderlust +gmail)
 
        :app
+       everywhere
        ;;calendar
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
